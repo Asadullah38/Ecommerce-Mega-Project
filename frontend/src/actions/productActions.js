97e8d5c7abd46431f3605212fd285b6110cad10f
Asadullah38 <2020se10@student.uet.edu.pk>
2022-10-05 01:18:42 +0500

    redux setup and backend connection

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { ALL_PRODUCT_FAIL, ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, CLEAR_ERRORS } from "../constants/productConstants";
+
+
+export const getProduct = () => async (dispatch) => {
+    try {
+        dispatch({ type: ALL_PRODUCT_REQUEST, })
+        const {data} = await axios.get(`/getProducts`)
+
+        dispatch({
+            type: ALL_PRODUCT_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        console.log(error)
+        dispatch({
+            type: ALL_PRODUCT_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+
+
+
+//CLEAR_ERRORS
+export const clearErrors = () => async (dispatch) => {
+    dispatch({ type: CLEAR_ERRORS, })
+}
+
+
--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -1,17 +1,33 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { CgMouse } from "react-icons/cg"
 import "./Home.css";
 import Footer from "../layout/footer/Footer";
 import Product from "./Product.jsx";
+import MetaData from '../layout/MetaData';
+import { useDispatch, useSelector } from "react-redux"
+import { getProduct } from '../../actions/productActions';
+import axios from 'axios';
 
 const Home = () => {
+    const dispatch = useDispatch();
+
     const product = {
         images: [{ url: "https://images.unsplash.com/photo-1620012253295-c15cc3e65df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHNoaXJ0fGVufDB8fDB8fA%3D%3D&w=1000&q=80" }],
         name: "Blue T-Shirt",
         price: "10.00$",
         _id: "asad"
     }
+
+
+    useEffect(() => {
+      dispatch(getProduct())
+
+    }, [dispatch])
+
+
+
     return (<>
+        <MetaData title="Ecommerce Red Store" />
         <div className="banner">
             <p>
                 Welcome to Ecommerce Red Store
@@ -20,7 +36,7 @@ const Home = () => {
                 FIND AMAZING PRODUCTS BELOW
             </h1>
             <a href="#container">
-                <button>scroll <CgMouse /></button>
+                <button >scroll <CgMouse /></button>
             </a>
         </div>
         <h2 className="homeHeading">Featured Products</h2>
